7474e621d84cb2cd99c0dae6c618f04827ee6d26
behoon <syauqeesy@gmail.com>
2023-09-02 10:47:46 +0700

    redesign home

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -4,25 +4,42 @@ function App() {
   return (
     <div className="app">
       <Navbar></Navbar>
-      <div className="">
-        <div className="">
-          <h1 className="">
-            <span className="">I&apos;m a Full-stack Developer</span>
-            <span className="">I love to build an amazing apps.</span>
+      <div className="p-2.5 lg:py-6 lg:px-20 lg:mt-20">
+        <div>
+          <h1>
+            <span className="block lg:font-bold lg:text-7xl lg:mb-10">
+              I&apos;m a Full-stack Developer,
+            </span>
+            <span className="block text-slate-800 lg:text-5xl">
+              I love to build amazing apps.
+            </span>
           </h1>
         </div>
-        <div className=""></div>
       </div>
 
-      <div className="">
-        <a href="https://instagram.com/syauqeesy" target="_blank">
-          <i className="bi bi-instagram cursor-pointer"></i>
+      <div className="p-2.5 lg:py-6 lg:px-20 lg:mt-10">
+        <a
+          href="https://instagram.com/syauqeesy"
+          className="underline"
+          target="_blank"
+        >
+          <i className="bi bi-instagram cursor-pointer lg:text-2xl"></i>{" "}
+          instagram.
         </a>
-        <a href="https://linkedin.com/in/syauqeesy" target="_blank">
-          <i className="bi bi-linkedin cursor-pointer"></i>
+        <a
+          href="https://linkedin.com/in/syauqeesy"
+          className="underline lg:ml-16"
+          target="_blank"
+        >
+          <i className="bi bi-linkedin cursor-pointer lg:text-2xl"></i>{" "}
+          LinkedIn.
         </a>
-        <a href="https://github.com/bee-hoon" target="_blank">
-          <i className="bi bi-github cursor-pointer"></i>
+        <a
+          href="https://github.com/bee-hoon"
+          className="underline lg:ml-16"
+          target="_blank"
+        >
+          <i className="bi bi-github cursor-pointer lg:text-2xl"></i> Github.
         </a>
       </div>
     </div>
