b5c8dfae682af62428221130c3662db95f269252
behoon <syauqeesy@gmail.com>
2023-09-02 10:27:40 +0700

    fix: breakpoint

--- application/src/component/Navbar.tsx
+++ application/src/component/Navbar.tsx
@@ -3,17 +3,17 @@ import NavbarItem from "./navbar/Item";
 
 function Navbar() {
   return (
-    <nav className="navbar p-2.5 md:py-6 md:px-20 bg-black">
+    <nav className="navbar p-2.5 lg:py-6 lg:px-20 bg-black">
       <ul className="flex justify-between">
         <NavbarItem className="font-bold" children="Ahmad Syauqi" />
 
-        <div className="hidden md:flex">
+        <div className="hidden lg:flex">
           <NavbarItem className="ml-10" children="experience." />
           <NavbarItem className="ml-10" children="work." />
           <NavbarItem className="ml-10" children="contact." />
         </div>
 
-        <div className="flex flex-col justify-center md:hidden">
+        <div className="flex flex-col justify-center lg:hidden">
           <NavbarItem>
             <NavbarDropdownButton />
           </NavbarItem>
