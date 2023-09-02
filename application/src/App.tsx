2903b0f6bffc03fcfd7c93783009542abbdc1775
behoon <syauqeesy@gmail.com>
2023-09-02 10:23:56 +0700

    fix: change breakpoint

--- application/src/App.tsx
+++ application/src/App.tsx
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="app">
       <Navbar></Navbar>
-      <div className="container flex mx-auto md:mt-44 md:mb-20">
+      <div className="container flex mx-auto lg:mt-44 lg:mb-20">
         <div className="grow">
           <h1 className="flex flex-col">
-            <span className="block md:font-semibold md:text-5xl md:mb-5">
+            <span className="block lg:font-semibold lg:text-5xl lg:mb-5">
               I&apos;m a Full-stack Developer
             </span>
-            <span className="block md:font-medium md:text-3xl">
+            <span className="block lg:font-medium lg:text-3xl">
               I love to build an amazing apps.
             </span>
           </h1>
@@ -23,10 +23,10 @@ function App() {
           <i className="bi bi-instagram cursor-pointer text-2xl"></i>
         </a>
         <a href="https://linkedin.com/in/syauqeesy" target="_blank">
-          <i className="bi bi-linkedin cursor-pointer text-2xl md:ml-10"></i>
+          <i className="bi bi-linkedin cursor-pointer text-2xl lg:ml-10"></i>
         </a>
         <a href="https://github.com/bee-hoon" target="_blank">
-          <i className="bi bi-github cursor-pointer text-2xl md:ml-10"></i>
+          <i className="bi bi-github cursor-pointer text-2xl lg:ml-10"></i>
         </a>
       </div>
     </div>
